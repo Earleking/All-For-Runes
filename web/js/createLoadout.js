674c3d30d49c35cd44d2ec2e2bf1451b777558f9
Earleking <arek.fielding@gmail.com>
2018-11-15 21:22:21 -0500

    runes display and are selected properly

--- web/js/createLoadout.js
+++ web/js/createLoadout.js
@@ -1,23 +1,32 @@
 // Define Global Consts
+var curRunes = new runesSeleted();
 
-function loadPrimaryTreeBasic(treeId) {
-    $.getJSON("./../../static/json/runes.json", (json) => {
-        for(var i of json) {
-            if(i["id"] == treeId) {
-                loadPrimaryTree(i);
-                // return;
+
+function loadSecondaryTree(treeId) {
+    curRunes.secondaryTree = treeId;
+    for(var tree of curRunes.allRunes) {
+        if(tree["id"] == treeId) {
+            for(var slot in tree["slots"]) {
+                if(slot == 0)
+                    continue;
+                loadLevel(tree["slots"][slot]["runes"], slot, false);
             }
         }
-        console.log("No Tree matched for primary");
-    });
+    }
+    console.log("No Tree matched for secondary");
 }
 
-function loadPrimaryTree(tree) {
-    console.log(tree)
-    for(var slot in tree["slots"]) {
-        loadLevel(tree["slots"][slot]["runes"], slot, true);
-        // return;
+
+function loadPrimaryTree(treeId) {
+    curRunes.primaryTree = treeId;
+    for(var tree of curRunes.allRunes) {
+        if(tree["id"] == treeId) {
+            for(var slot in tree["slots"]) {
+                loadLevel(tree["slots"][slot]["runes"], slot, true);
+            }
+        }
     }
+    console.log("No Tree matched for primary");    
 }
 
 function loadLevel(slot, i, primaryTree) {    
@@ -33,11 +42,29 @@ function loadLevel(slot, i, primaryTree) {
         var img = document.createElement("img");
         img.src = runePath + rune["id"] + ".png";
         img.classList.add("rune");
+        img.id = rune["id"];
+        if(i != 0)
+            img.classList.add("sub-rune");
         master.appendChild(img);
     }
+}
 
+function selectNewRune(newId) {
+    var oldRune = curRunes.addRune(newId);
+    document.getElementById(newId).classList.add("selected");
+    if(oldRune == 0)
+        return;
+    document.getElementById(oldRune).classList.remove("selected");
 }
 
 $(document).ready(() => {
-    loadPrimaryTreeBasic(8100);
+    $.getJSON("./../../static/json/runes.json", (json) => {
+        curRunes.allRunes = json;
+        loadPrimaryTree(8100);
+        loadSecondaryTree(8200);
+        $("body").on("click", ".rune", (event) => {
+            selectNewRune(event["currentTarget"]["id"]);
+        });
+    });
 });
+
---
+++ web/js/runesSelected.js
@@ -0,0 +1,100 @@
+class runesSeleted {
+    constructor() {
+        this.allRunes = {};
+        this.primaryTree = 0;
+        this.secondaryTree = 0;
+        this.firstSecondary = 0;
+        this.primaryRunes = {
+            "0": 0,
+            "1": 0,
+            "2": 0,
+            "3": 0
+        }
+        this.secondaryRunes = {
+            "1": 0,
+            "2": 0,
+            "3": 0
+        }
+    }
+
+    fullSecondary() {
+        var freeSlots = 0;
+        for(var r in this.secondaryRunes) {
+            if(this.secondaryRunes[r] == 0)
+                freeSlots += 1;
+        }
+        if(freeSlots > 1)
+            return false;
+        return true;
+    }
+    
+    addRune(newRune) {
+        for(var tree of this.allRunes) {
+            // Check primary tree
+            if(tree["id"] == this.primaryTree) {
+                for(var slotI in tree["slots"]) {
+                    var slot = tree["slots"][slotI];
+                    for(var runeI in slot["runes"]) {
+                        var rune = slot["runes"][runeI];
+                        if(rune["id"] == newRune) {
+                            // Rune is in primary tree in slot
+                            var oldRune = this.primaryRunes[slotI];
+                            if(oldRune == newRune) {
+                                this.primaryRunesw[slotI] = 0;
+                                return oldRune;
+                            }
+                            this.primaryRunes[slotI] = newRune;
+                            return oldRune;
+                        }
+                    }
+                }
+            }
+
+            // check seecondary tree
+            if(tree["id"] == this.secondaryTree) {
+                for(var slotI in tree["slots"]) {
+                    var slot = tree["slots"][slotI];
+                    for(var runeI in slot["runes"]) {
+                        var rune = slot["runes"][runeI];
+                        if(rune["id"] == newRune) {
+                            // we found your new rune
+
+                            if(this.firstSecondary == 0) {
+                                this.firstSecondary = slotI;
+                            }
+                            // Rune is in primary tree in slot
+                            var oldRune = this.secondaryRunes[slotI];
+
+                            // If the already selected rune is clicked, deselect
+                            if(oldRune == newRune) {
+                                this.secondaryRunes[slotI] = 0;
+                                return oldRune;
+                            }
+
+                            // Check if 2 are already selected
+                            if(this.fullSecondary() && oldRune == 0) {
+                                // console.log(this.firstSecondary);
+                                oldRune = this.secondaryRunes[this.firstSecondary];
+                                this.secondaryRunes[this.firstSecondary] = 0;
+                                for(var i in this.secondaryRunes) {
+                                    if(i != this.firstSecondary && i != slotI) {
+                                        this.firstSecondary = i;
+                                        // Set new rune
+                                        this.secondaryRunes[slotI] = newRune;
+                                        return oldRune; 
+                                    }
+                                }
+                                // console.log('fdsaf');
+                            } 
+
+                            this.lastSecondary = slotI;
+                            // Set new rune
+                            this.secondaryRunes[slotI] = newRune;
+                            return oldRune;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
